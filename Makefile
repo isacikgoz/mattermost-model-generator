bbe9fdae48074c22515b99305a00a20ad44869e3
Eli Yukelzon <reflog@gmail.com>
2020-05-21 16:07:53 +0300

    changed migrate command syntax, write output

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@
 generate:
 	go generate
 	gofmt -s -w ./output/.
+
+migrate_channel:
+	go run ./cmd/migrator model Channel  ../mattermost-server/**/*.go
--- cmd/migrator/main.go
+++ cmd/migrator/main.go
@@ -1,35 +1,49 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"go/ast"
+	"go/format"
 	"go/parser"
 	"go/token"
+	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/grundleborg/mattermost-model-generator/internal/finder"
 )
 
-// go run ./cmd/migrator $GOPATH/src/github.com/mattermost/mattermost-server/**/*.go
+// go run ./cmd/migrator model Post $GOPATH/src/github.com/mattermost/mattermost-server/**/*.go
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Fprintln(os.Stderr, "not enough program arguments")
+	if len(os.Args) < 4 {
+		fmt.Fprintln(os.Stderr, "not enough program arguments: [cmd] [package] [model] [files to scan]")
 		os.Exit(1)
 	}
+	packageName := os.Args[1]
+	modelName := os.Args[2]
+	fset := token.NewFileSet()
 
-	fs := token.NewFileSet()
-
-	for _, arg := range os.Args[1:] {
-		f, err := parser.ParseFile(fs, arg, nil, parser.AllErrors)
+	for _, fileName := range os.Args[3:] {
+		if strings.Contains(fileName, "mattermost-server/vendor") {
+			continue
+		}
+		fileNode, err := parser.ParseFile(fset, fileName, nil, parser.AllErrors|parser.ParseComments)
 		if err != nil {
-			log.Printf("could not parse %s: %v", arg, err)
+			log.Printf("could not parse %s: %v", fileName, err)
 			continue
 		}
 		w := &finder.Walker{
-			Name:    "Post",
-			Package: "model",
+			Name:    modelName,
+			Package: packageName,
 		}
-		ast.Walk(w, f)
+		ast.Walk(w, fileNode)
+		buf := new(bytes.Buffer)
+		if err = format.Node(buf, fset, fileNode); err != nil {
+			log.Printf("error: %v\n", err)
+		} else if err := ioutil.WriteFile(fileName, buf.Bytes(), 0664); err != nil {
+			log.Printf("error: %v\n", err)
+		}
 	}
 }
